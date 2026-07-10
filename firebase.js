3398630d2eb59b9b802d370560136c7f94a54f9f
Raghadmohamad18 <raghadraghadmohamad@gmail.com>
2026-07-10 23:08:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,6 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyC3I9DDJKU2D5T3JMPwyoMbVzOpi8KisSQ",
   authDomain: "skillmasr.firebaseapp.com",
@@ -12,10 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "290650747699",
   appId: "1:290650747699:web:638916dc7ecee74ebfc446"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export Services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
